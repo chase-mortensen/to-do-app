28f645d37de64a325cd6be61da52a9c99afc5c47
Chase Mortensen <chase.mortensen3@gmail.com>
2020-08-24 00:41:31 -0600

    user interface feeling better, need to add the filters next

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -25,20 +25,26 @@
           <v-list>
             <v-text-field
             solo
-            placeholder="Enter item here"
+            placeholder="Enter new item here"
             v-model="newItem"
             @keyup.enter="addItem"
           ></v-text-field>
             <v-list-item
               v-for="item in todoItems.filter(item => !item.complete)"
               :key="item.id"
-              @click="console.log()"
             >
               <v-list-item-avatar>
                 <v-checkbox v-model="item.complete"></v-checkbox>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-text-field
+                :value="item.title"
+                solo
+                flat
+                :outlined="item.editing"
+                @click="item.editing=true"
+                @blur="item.editing=false"
+                ></v-text-field>
               </v-list-item-content>
               <v-list-item-icon>
                 <div class="my-2">
@@ -52,15 +58,24 @@
             <v-list-item
               v-for="item in todoItems.filter(item => item.complete)"
               :key="item.id"
-              @click="console.log()"
             >
               <v-list-item-avatar>
                 <v-checkbox v-model="item.complete"></v-checkbox>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title 
+                <!-- <v-list-item-title 
                 style="text-decoration: line-through"
                 v-text="item.title"></v-list-item-title>
+                <p>{{ item.editing }}</p> -->
+                <v-text-field
+                :style="item.editing ? '' : 'text-decoration: line-through'"
+                :value="item.title"
+                solo
+                flat
+                :outlined="item.editing"
+                @click="item.editing=true"
+                @blur="item.editing=false"
+                ></v-text-field>
               </v-list-item-content>
               <v-list-item-icon>
                 <div class="my-2">
@@ -81,10 +96,10 @@
       newItem: '',
       id: 4,
       todoItems: [
-        { id: 0, complete: true, title: 'item 1' },
-        { id: 1, complete: true, title: 'item 2' },
-        { id: 2, complete: false, title: 'item 3', },
-        { id: 3, complete: false, title: 'item 4' },
+        { id: 0, editing: false, complete: false, title: 'item 1' },
+        { id: 1, editing: false, complete: false, title: 'item 2' },
+        { id: 2, editing: false, complete: true, title: 'item 3', },
+        { id: 3, editing: false, complete: true, title: 'item 4' },
       ],
     }),
     methods: {
@@ -93,7 +108,7 @@
           this.todoItems.push({ id: this.id++, complete: false, title: this.newItem });
           this.newItem = '';
         }
-      }
+      },
     }
   }
 </script>
