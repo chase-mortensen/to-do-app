e42cf058809b89aa4aaef44ccddb1a3e45798bc5
Chase Mortensen <chase.mortensen3@gmail.com>
2020-08-25 00:24:18 -0600

    finishing the local version - CRUD working & filtering complete, pending, and all items working properly using 'data'

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -19,9 +19,32 @@
             color="deep-purple accent-4"
             dark
           >
-            <v-toolbar-title>to-do-app</v-toolbar-title>
+            <v-toolbar-title>to-do-app <small>(click on list items to edit)</small></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn 
+              small 
+              outlined
+              color="normal"
+              @click="listState='all'"
+            >All</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn 
+              small 
+              outlined
+              color="normal"
+              @click="listState='pending'"
+            >Pending</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn 
+              small 
+              outlined
+              color="normal"
+              @click="listState='complete'"
+            >Complete</v-btn>
+
           </v-toolbar>
           
+          <div v-if="listState !== 'complete'">
           <v-list>
             <v-text-field
             solo
@@ -37,24 +60,33 @@
                 <v-checkbox v-model="item.complete"></v-checkbox>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-text-field
-                :value="item.title"
-                solo
-                flat
+                <v-textarea
+                :value="item.text"
+                :shaped="item.editing"
+                :filled="item.editing"
+                auto-grow
+                rows="1"
                 :outlined="item.editing"
-                @click="item.editing=true"
+                @click="focusItem(item)"
+                @input="setText(item, $event)"
                 @blur="item.editing=false"
-                ></v-text-field>
+                ></v-textarea>
               </v-list-item-content>
               <v-list-item-icon>
                 <div class="my-2">
-                  <v-btn small color="error">Delete</v-btn>
+                  <v-btn class="mx-2" fab dark small 
+                  color="error"
+                  @click="removeItem(item.id)"
+                  >
+                    <v-icon dark>mdi-delete</v-icon>
+                  </v-btn>
                 </div>
               </v-list-item-icon>
             </v-list-item>
           </v-list>
-          <v-divider></v-divider>
-          <v-list>
+          </div>
+          <v-divider v-if="separatorNeeded"></v-divider>
+          <v-list v-if="listState !== 'pending'">
             <v-list-item
               v-for="item in todoItems.filter(item => item.complete)"
               :key="item.id"
@@ -65,21 +97,29 @@
               <v-list-item-content>
                 <!-- <v-list-item-title 
                 style="text-decoration: line-through"
-                v-text="item.title"></v-list-item-title>
+                v-text="item.text"></v-list-item-title>
                 <p>{{ item.editing }}</p> -->
-                <v-text-field
+                <v-textarea
                 :style="item.editing ? '' : 'text-decoration: line-through'"
-                :value="item.title"
-                solo
-                flat
+                :value="item.text"
+                :shaped="item.editing"
+                :filled="item.editing"
+                auto-grow
+                rows="1"
                 :outlined="item.editing"
-                @click="item.editing=true"
-                @blur="item.editing=false"
-                ></v-text-field>
+                @click="focusItem(item)"
+                @input="setText(item, $event)"
+                @blur="item.editing = false"
+                ></v-textarea>
               </v-list-item-content>
               <v-list-item-icon>
                 <div class="my-2">
-                  <v-btn small color="error">Delete</v-btn>
+                  <v-btn class="mx-2" fab dark small 
+                    color="error"
+                    @click="removeItem(item.id)"
+                  >
+                    <v-icon dark>mdi-delete</v-icon>
+                  </v-btn>
                 </div>
               </v-list-item-icon>
             </v-list-item>
@@ -93,22 +133,42 @@
 <script>
   export default {
     data: () => ({
+      listState: 'all',
       newItem: '',
       id: 4,
       todoItems: [
-        { id: 0, editing: false, complete: false, title: 'item 1' },
-        { id: 1, editing: false, complete: false, title: 'item 2' },
-        { id: 2, editing: false, complete: true, title: 'item 3', },
-        { id: 3, editing: false, complete: true, title: 'item 4' },
+        { id: 0, editing: false, complete: false, text: 'item 1' },
+        { id: 1, editing: false, complete: false, text: 'item 2' },
+        { id: 2, editing: false, complete: true, text: 'item 3', },
+        { id: 3, editing: false, complete: true, text: 'item 4' },
       ],
     }),
+    computed: {
+      separatorNeeded() {
+        const len = this.todoItems.filter(item => item.complete).length;
+        return !(this.todoItems.length === len || len === 0);
+      }
+    },
     methods: {
       addItem() {
         if (this.newItem.trim()) {
-          this.todoItems.push({ id: this.id++, complete: false, title: this.newItem });
+          this.todoItems.push({ id: this.id++, complete: false, text: this.newItem });
           this.newItem = '';
         }
       },
+      removeItem(id) {
+        this.todoItems = this.todoItems.filter(item => item.id !== id)
+        console.log(this.todoItems);
+      },
+      focusItem(item) {
+        console.log('focusing...');
+        item.editing = true;
+      },
+      setText(item, event) {
+        if (event) {
+          item.text = event;
+        }
+      }
     }
   }
 </script>
